0332f413f782f4fc953d4c39886497658a7fb1b8
Chris Desch <cdesch@gmail.com>
2015-04-26 13:08:41 -0400

    Initial Commit

---
+++ SueC2L6OneFile/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SueC2L6OneFile
+//
+//  Created by cj on 4/26/15.
+//  Copyright (c) 2015 KickinEspresso. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
